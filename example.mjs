dcbc1decbf05ac4471955146fa2085e198a2b0b8
Coxxs <coxxs@users.noreply.github.com>
2022-11-22 04:36:59 +0800

    Update example

--- example.mjs
+++ example.mjs
@@ -1,7 +1,8 @@
 import { promises as fs } from 'fs'
 import { updatePchtxt } from "./port.mjs"
 
-// 1. Decompress NSO using hactool: hactool -t nso main --uncompressed main.decompressed
+// 1. Install [Node.js](https://nodejs.org/)
+// 2. Decompress NSO using hactool: hactool -t nso main --uncompressed main.decompressed
 let fileOld = await fs.readFile('1.2.0.decompressed')
 let fileNew = await fs.readFile('1.2.1.decompressed')
 
@@ -10,6 +11,6 @@ let newPchtxt = updatePchtxt(fileOld, fileNew, oldPchtxt)
 
 fs.writeFile('patch_1.2.1.pchtxt', newPchtxt)
 
-// 3. Use node.js to run this script: node example.js
+// 3. Use Node.js to run this script: node example.mjs
 // 4. Search for "[!]" in new pchtxt to find errors
 // 5. Update nsobid in new pchtxt manually
